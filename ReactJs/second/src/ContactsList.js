1a89cc7f9c019123278bbfc07774988b62ac81c1
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2016-04-22 08:26:18 -0700

    Controlled inputs using React 	- Adding input search field

--- ReactJs/second/src/ContactsList.js
+++ ReactJs/second/src/ContactsList.js
@@ -2,14 +2,32 @@ import React from 'react';
 import Contact from './Contact';
 
 class ContactsList extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			search: 'ItsABug'
+		};
+	}
+
+	updateSearch(event) {
+		console.log('Yoh ! : ' + event.target.value);
+		this.setState({search:event.target.value.substr(0,20)});
+	}
+
 	render() {
 		console.log(this.props.contacts);
 		return (
+			<div> 
 				<ul>
 					{this.props.contacts.map((contact)=> {
 						return <Contact contact={contact} key={contact.id} /> 
 					})}
 				</ul>
+
+				<input type="text" 
+					value= {this.state.search} 
+					onChange={this.updateSearch.bind(this)} />
+			</div>
 			)
 	}
 }
