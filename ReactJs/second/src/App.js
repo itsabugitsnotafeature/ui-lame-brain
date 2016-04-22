e01efd2eabcfadf3d5d32f2e4d3a1254fe4c7e34
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2016-04-22 08:11:27 -0700

    Iterating over array of data 	- Also adding another sub component. 	- Iterating over Array of data 	- Using map() function for ES5 	- Adding another ID property to Array

--- ReactJs/second/src/App.js
+++ ReactJs/second/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ContactsList from './ContactsList';
 
-let contacts = {
+let contacts = [{
+	id: 1,
 	name: 'ItsABug',
 	phone: '555 333 7777'
-};
+}, {
+	id: 2,
+	name: 'ItsNot',
+	phone: '111 222 3333'
+}, {
+	id: 3,
+	name: 'Afeature',
+	phone: '444 555 6666'
+}];
 
 class App extends React.Component {
 	render() {
