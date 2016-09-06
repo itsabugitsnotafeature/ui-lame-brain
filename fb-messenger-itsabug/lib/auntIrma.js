40f916f6cc45cafb35a5477cbcff8a05b383cae3
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2016-09-05 20:34:35 -0700

    Introducing aunt irma. 	- Ability to calculate next 12 occurrences from one control date-time occurrence.

---
+++ fb-messenger-itsabug/lib/auntIrma.js
@@ -0,0 +1,203 @@
+'use strict';
+const log = require('./log');
+const Regex = require("regex");
+const fetch = require('node-fetch');
+
+let myLogger = new log.Logger(log.DEBUG);
+let dateRegEx = "<h2>Result: (.*)<\/h2>";
+
+/*
+	Template : 
+		http://www.timeanddate.com/date/dateadded.html?m1=_controlMonth_12&d1=_controlDate_22&y1=_controlYear_&type=add&ay=&am=&aw=&ad=_controlCyclePeriod_
+
+	Live Examples : 
+      // Incorrect data 
+		1. http://www.timeanddate.com/date/dateadded.html?m1=08'&d125=&y1=2016&type=add&ay=&am=&aw=&ad=26'
+			Result String "<h2>Result: Thursday, September 1, 2016</h2>"
+
+      // Correct data 
+		2. http://www.timeanddate.com/date/dateadded.html?m1=8&d1=25&y1=2016&type=add&ay=&am=&aw=&ad=26&rec='
+			Result String "<h2>Result: Tuesday, September 20, 2016</h2>"
+      <h2>Result: Tuesday, September 20, 2016</h2>
+		2. 
+
+	Code Example : 
+		const DATE_API_URL = 'http://www.timeanddate.com/date/dateadded.html?m1=' + 
+								controlMonth.toString() + 
+								'&d1=' + 
+								controlDate.toString() + 
+								'&y1=' + 
+								controlYear.toString() + 
+								'&type=add&ay=&am=&aw=&ad=' + 
+								controlCyclePeriod.toString();
+
+*/ 
+
+
+class Irma {
+
+  /*
+		Constructor : Returns new object initialized with master data, month, year, cycle time and visitDuration
+    Default returns mater date time 
+	*/ 
+    constructor(date=25, month=8, year=2016, cycle=26, visitDuration=5) {
+        this.controlDate 		= date;
+        this.controlMonth 		= month;
+        this.controlYear 		= year;
+		    this.controlCyclePeriod = cycle;
+        this.daysOfVisit 		= visitDuration;
+    }
+
+    getDate(url) {
+      return fetch(url)
+      .then(rsp => rsp.text() )
+      .then(body => {
+        return body.match(/<h2>Result: (.*)<\/h2>/g).toString().replace(/<h2>Result: /gm,"").replace(/<\/h2>/gm,"");
+      });
+    }
+
+    showControlParams() {
+        let thisUser = this;
+        myLogger.debug("showControlParams : Called.");
+        myLogger.debug(`\nDate:${thisUser.controlDate},\nMonth: ${thisUser.controlMonth},\nYear: ${thisUser.controlYear},\nCycle Period ${thisUser.controlCyclePeriod},\nDays Of Visit: ${thisUser.daysOfVisit}\n`);
+    }
+
+    getDeltaDateApi(delta) {
+        let thisUser = this;
+        // myLogger.debug("getDeltaDateApi : Called.");
+
+        return 'http://www.timeanddate.com/date/dateadded.html?m1=' + 
+               thisUser.controlMonth.toString() + 
+               '&d1=' + 
+               thisUser.controlDate.toString() + 
+               '&y1=' + 
+               thisUser.controlYear.toString() + 
+               '&type=add&ay=&am=&aw=&ad=' + 
+               delta.toString();
+    }
+    getPredictionsArray() {
+      let thisUser = this;
+      let predictionsArray = new Array();
+      let i = 1;
+      let doublyMonthDetected = false;
+
+      return new Promise(function(resolve, reject) {
+        for( let i = 1 ; i < 13 ; i++ ) {
+          let delta = ( i * thisUser.controlCyclePeriod );
+          let deltaUrl = thisUser.getDeltaDateApi(delta);
+          thisUser.getDate(deltaUrl.toString())
+            .then(date => {
+              let datapoint = {};
+              datapoint[i] = date;
+              predictionsArray.push(datapoint);
+              if ( predictionsArray.length === 12 ) {
+                return resolve(predictionsArray);  
+              }
+          })
+        }
+      });
+    }
+    calculateFutureVisits() {
+      let thisUser = this;
+      myLogger.debug("calculateFutureVisits : Called.");
+
+    	let yearlyPredictions = {};
+    	let predictions = {};
+    	yearlyPredictions.VisitMap = new Array();
+      
+      thisUser.getPredictionsArray()
+        .then(herPredictions => {
+          yearlyPredictions.VisitMap = herPredictions;
+          myLogger.debug("List of her predictions..." );
+          myLogger.debug( JSON.stringify(yearlyPredictions, 2, null) );
+        });
+    }
+}
+
+const inst = new Irma();
+inst.calculateFutureVisits();
+myLogger.debug("Okay I ended." ); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+Example of date time json structure : 
+{
+  "VisitMap": [
+    {
+      "2": "Sunday, October 16, 2016"
+    },
+    {
+      "4": "Wednesday, December 7, 2016"
+    },
+    {
+      "3": "Friday, November 11, 2016"
+    },
+    {
+      "8": "Tuesday, March 21, 2017"
+    },
+    {
+      "1": "Tuesday, September 20, 2016"
+    },
+    {
+      "12": "Monday, July 3, 2017"
+    },
+    {
+      "5": "Monday, January 2, 2017"
+    },
+    {
+      "10": "Friday, May 12, 2017"
+    },
+    {
+      "6": "Saturday, January 28, 2017"
+    },
+    {
+      "9": "Sunday, April 16, 2017"
+    },
+    {
+      "11": "Wednesday, June 7, 2017"
+    },
+    {
+      "7": "Thursday, February 23, 2017"
+    }
+  ]
+}
+
+
+DATE Api Reference
+var date = new Date(1313564400000);
+var month = date.getMonth();
+OUTPUT : 7
+
+*/
+
+
+
+
